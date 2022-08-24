9299817e8aa9bbef4558f4a4df6d0bb491220dc4
Vitalii-Zh-83 <108428014+Vitalii-Zh-83@users.noreply.github.com>
2022-08-24 22:35:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* alert("nf zzrjuj [ez") */
+const number1 = prompt ("Скільки бабла насипала мама?");
+/* promt це така ж фігня (метод) як алерт, але виводить поле для внесення данних */
+parseFloat(number1);
+const number2 = prompt ("Скільки бабла тобі насипав папа?");
+parseFloat(number2);
+alert(parseFloat(number1)+parseFloat(number2)+" грн. Тобі цього вистачить?");
